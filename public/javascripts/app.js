910f360c63e7185630ab1bc469b44bafef04db0f
oatItsaraphong <itsaraphong@csu.fullerton.edu>
2016-10-30 19:46:07 -0800

    first commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,184 @@
+/* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, maxlen: 80, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
+
+var waitClick;
+var AddQuestionFunction;
+var AnswerQuestionOne;
+var GetScore;
+var GetQuestionNew;
+
+//score display
+var ScoreUpdate = function(scoreObject){
+  'use strict';
+  $('.ScoreCorrect').empty();
+  $('.ScoreCorrect').append(
+    '<div><h3>Correct: ' + scoreObject.right + '</h3></div>'
+  );
+
+  $('.ScoreIncorrect').empty();
+  $('.ScoreIncorrect').append(
+    '<div><h3>Wrong: ' + scoreObject.wrong + '</h3></div>'
+  );
+};//end ScoreUpdate
+//dispay a new question
+var AdjustQuestion = function(questionObject){
+  'use strict';
+  $('.oneQuestion').empty();
+  $('.oneQuestion').append(
+
+    '<div class="field NewQuestion">' +
+    '<label class="questionPlace"><h3>' +
+    questionObject.question +
+    '</h3></label>' +
+    '<lable class="idPlace">' +
+    questionObject.answerId +
+    '</label>' +
+    '<input type="text" class="answerClass" placeholder="answer">' +
+    '</div>' +
+    '<button class="ui button fluid teal large ToAnswer"' +
+    'id="AnswerToThis" type="button">Answer</button>'
+  );
+  waitClick();
+};//end AdjustQuestion
+
+//function that always listening
+$(function(){
+  'use strict';
+  $('.addQuestion').form({
+    fields: {
+        addquestionholder: {
+          identifier : 'addquestion-holder',
+            rules: [
+              {
+                type   : 'empty',
+                prompt : 'Please enter a question'
+              }
+            ]
+          },
+          addanswerholder: {
+              identifier : 'addanswer-holder',
+              rules: [
+                {
+                  type   : 'empty',
+                  prompt : 'Please enter a answer'
+                }
+                ]
+            }
+        },
+    onSuccess: function(event) {
+      event.preventDefault();
+      AddQuestionFunction();
+      $('.newQuestion').val('');
+      $('.newAnswer').val('');
+      //GetQuestionNew();
+      console.log('form valid');
+    }
+  });
+  GetQuestionNew();
+});//big loop
+
+//ajax when answer the question
+function AnswerQuestionOne(){
+  'use strict';
+  var aToSend = $('.answerClass').val();
+  var idToSend = $('.idPlace').text();
+  var jsonStr = JSON.stringify({
+                  'answer': aToSend.toUpperCase(), 'answerId': idToSend});
+  console.log(jsonStr);
+  $.ajax({
+          type: 'POST',
+          data: jsonStr,
+          dataType: 'json',
+          contentType: 'application/json',
+          url: 'http://localhost:3000/answer',
+          success: function(data){
+            console.log(data);
+            if(data.correct === true){
+              console.log('Correct');
+            }
+            else{
+              console.log('Wrong');
+            }
+          }
+  });
+
+}//end AnswerQuestion
+
+//ajax when the score need update
+function GetScore(){
+  'use strict';
+  console.log('retieve score');
+  $.ajax({
+          type: 'GET',
+          dataType: 'json',
+          contentType: 'application/json',
+          url: 'http://localhost:3000/score',
+          success: function(data){
+            console.log(data);
+            ScoreUpdate(data);
+          }
+  });
+}//end GetScore
+
+//ajax retive one question get it at randome by server
+function GetQuestionNew(){
+  'use strict';
+  console.log('retieve question');
+  $.ajax({
+          type: 'GET',
+          dataType: 'json',
+          contentType: 'application/json',
+          url: 'http://localhost:3000/question',
+          success: function(data){
+            console.log('success retirve');
+            console.log(data);
+
+            if(data.answerId === 0)
+            {
+              $('.oneQuestion').empty();
+              $('.oneQuestion').append('<h3>Add the question first</h3>' +
+                '<div><button class="ui grey button"href="localhost:3000">' +
+                'Click when question is added</button></div>'
+              );
+            }
+            else{
+              AdjustQuestion(data);
+              GetScore();
+            }
+          }
+    });
+
+}//end GetQuestionNew
+
+//ajax added question to the db
+function AddQuestionFunction(){
+  'use strict';
+
+  var qToSend = document.getElementsByName('addquestion-holder')[0].value;
+  var aToSend = document.getElementsByName('addanswer-holder')[0].value;
+  var jsonStr = JSON.stringify({'question': qToSend,
+                                  'answer': aToSend.toUpperCase()});
+  console.log(jsonStr);
+
+  $.ajax({
+          type: 'POST',
+          data: jsonStr,
+          dataType: 'json',
+          contentType: 'application/json',
+          url: 'http://localhost:3000/question',
+          success: function(data){
+            console.log('send quesition complete');
+          }
+
+  });
+
+}//end AddQuestionFunction
+
+//wait for user to answer
+function waitClick(){
+  'use strict';
+  $('#AnswerToThis').click(function(){
+    console.log('ToAnswer');
+    AnswerQuestionOne();
+    GetQuestionNew();
+  });
+}
---
+++ server.js
@@ -0,0 +1,195 @@
+/* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, maxlen: 80, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
+
+//include requirement
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var mongoose = require('mongoose');
+var redis = require('redis');
+
+
+//connect to mongo database
+mongoose.connect('mongodb://localhost/TriviaGame');
+mongoose.set('debug', true)
+
+//redis connection
+client = redis.createClient();
+client.on('connect', function() {
+    console.log('connected');
+});
+
+//init object
+var questionSchema = new mongoose.Schema({
+  questionString: String,
+  answerString: String,
+  answerID: Number
+});
+
+
+var QuestionDb = mongoose.model('questionCollection', questionSchema);
+
+var app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.use("/", express.static("public"));
+console.log('!---- Trivia Game server side, listening at port 3000 ----!');
+
+//----Function Code------------------------------------------
+
+//set defautl and create redis
+setDefaultScore = function() {
+	client.set('ScoreRight','0', function(err, reply) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Key "ScoreRight" added');
+		}
+	});
+	client.set('ScoreWrong','0', function(err, reply) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Key "ScoreWrong" added');
+		}
+	});
+};
+
+//return score from redis
+app.get('/score', function(req,res){
+  console.log('score');
+
+  client.get('ScoreRight',function(err, replyRight){
+      console.log('up: ' + replyRight);
+
+      client.get('ScoreWrong',function(err, replyWrong){
+          console.log('down: ' + replyWrong);
+
+            res.json({'right': replyRight, 'wrong' : replyWrong});
+        });
+    });
+});
+
+//get the answer from user and compare it then return t/f
+app.post('/answer', function(req,res){
+  console.log('answering');
+  var answerIn = req.body.answer;
+  var idIn = req.body.answerId;
+
+  QuestionDb.find({answerID:idIn},{answerID:1, answerString: 1}).exec(
+    function(err, dataIn){
+
+      //compare two sting return 0 if exactly the same
+      var result = dataIn[0].answerString.localeCompare(answerIn);
+      console.log('compare: ' + result);
+      if(result === 0){
+        //update score
+        client.incr('ScoreRight',function(err, replay){
+          if(err){
+            console.log('Error update Score');
+          }
+          else{
+            console.log('New Rigth Score: ' + replay);
+          }
+        });
+        //return
+        res.json({'correct': true});
+      }//end if
+      else {
+        //update score
+        client.incr('ScoreWrong',function(err, replay){
+          if(err){
+            console.log('Error update Score');
+          }
+          else{
+            console.log('New Wrong Score: ' + replay);
+          }
+        });
+        //return
+        res.json({'correct': false});
+      }//end else
+    });//end exec
+});// end of answer
+
+//return one question at random to the user
+app.get('/question', function(req, res){
+  console.log('Retrive Question');
+
+  //initialize score one load
+  client.get('ScoreRight',function(err, reply){
+    if(reply=== null){
+      setDefaultScore();
+      console.log('up: ' + reply);
+    }
+  });
+
+  //RNG
+  var test;
+  QuestionDb.find({},{answerID:1}).exec(
+    function(err, dataCount){
+    test = Math.floor(Math.random() * (dataCount.length - 1) + 1);
+
+    //retrive question
+    QuestionDb.find({answerID:test}).exec(
+                    function(err, dataIn){
+
+        if(dataIn.length === 0)
+        {
+          console.log('No Question');
+          //return 0 if their is no data in DB
+          res.json({'question': 'No Question in DB', 'answerId':0});
+        }
+        else {
+
+          res.json({'question': dataIn[0].questionString,
+            'answerId':dataIn[0].answerID});
+        }
+      });//end retrive question
+
+  });//end RNG
+});
+
+//Adding new question
+app.post('/question', function(req, res){
+  console.log('Add Question Function');
+
+  var questionIn = req.body.question;
+  var answerIn = req.body.answer;
+  var result = 1;
+
+  //Retrive that data with the highest answerID : to assign the unqiue one
+  //  to the new question
+  QuestionDb.find({answerID:{$exists:true}},
+                  {answerID:1}).sort({answerID:-1}).limit(1).exec(
+    function(err, dataIn){
+
+      //check for empty database
+      if(dataIn.length !== 0){
+        console.log('Number of Question in DB and ID: ' + dataIn[0].answerID);
+        result = dataIn[0].answerID + 1;
+      }
+      else{
+        result = 1;
+      }
+
+      //initialize the dataset
+      var q1 = new QuestionDb({questionString: questionIn,
+                      answerString: answerIn,
+                      answerID: result});
+      //save dataset
+      q1.save(function(err, result){
+        if(err){
+          console.log('Error Save to DB');
+          res.json('Error Saving to DB');
+        }
+        else{
+          console.log('Question Added');
+          res.json('Question Added')
+        }
+      });//save function
+    });//end DB find exec
+});//end /quesiton post
+
+app.listen(3000);
